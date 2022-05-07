fff1b567719a5748a2fb02a399987cc2ef0beca0
oratake <smith.shimomura@gmail.com>
2022-05-07 20:49:45 +0900

    feat: タイトルと見出し仮設置

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+
+import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <Button variant="primary">Hoge</Button>
+            <Container fluid="lg">
+                <Row className="flex-nowrap justify-content-between align-items-center py-3 border-bottom">
+                    <Col>
+                        <h1>真秀書院</h1>
+                    </Col>
+                </Row>
+                <Nav>
+                    <Nav.Item>
+                        <Nav.Link href="#overview">真秀書院について</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#classroom">仙台教室</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#online">オンライン教室</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Container>
         </div>
     );
 }
